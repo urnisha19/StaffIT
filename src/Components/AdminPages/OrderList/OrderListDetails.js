afdfebe67345243727ba56a7bd3aaad0e9e2455c
urnisha19 <urnisha09@gmail.com>
2021-05-21 17:53:47 +0600

    complete

--- src/Components/AdminPages/OrderList/OrderListDetails.js
+++ src/Components/AdminPages/OrderList/OrderListDetails.js
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
+import React, { useState } from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-;
 
 const OrderListDetails = ({ order }) => {
     const [status, setStatus] = useState('');
--- src/Components/HomePage/TopBanner/NavBar.js
+++ src/Components/HomePage/TopBanner/NavBar.js
@@ -1,18 +1,25 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import logo from '../../../images/staffIT.png';
 import './NavBar.css';
-import { UserContext } from '../../../App';
 
 const NabBar = () => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const name = JSON.parse(localStorage.getItem("name"));
+    const email = JSON.parse(localStorage.getItem("email"));
+
+    const history = useHistory();
+
+    const handleLogOut = () => {
+        localStorage.clear();
+        history.go(0);
+    }
 
     return (
         <Container>
             <Navbar bg="***" expand="lg" className="py-4">
                 <Link to="/home">
-                    <img src={logo} alt="" className="logo w-25" />
+                    <img src={logo} alt="" className="logo w-25 px-3" style={{ height:"75px"}}/>
                 </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -21,13 +28,10 @@ const NabBar = () => {
                         <Link to="/customer/serviceBooking" className="navBar-link">Get Our Services</Link>
                         <Link to="/admin/orderList" className="navBar-link">Admin</Link>
                         <Link to="#" className="navBar-link">Contact Us</Link> 
-                        <Link to="#"><span style={{color:"paleVioletRed"}}>{loggedInUser.displayName}</span></Link>
+                        <Link to="#"><span style={{color:"paleVioletRed"}}>{name}</span></Link>
                         {
-                            loggedInUser.email ?
-                                <Button className="navBar-button" onClick={() =>{
-                                    setLoggedInUser({});
-                                    sessionStorage.removeItem('token')
-                                } }>Logout</Button>
+                            email ?
+                                <Button className="navBar-button" onClick={handleLogOut}>Logout</Button>
                                 :
                                 <Link to="/login">
                                     <Button className="navBar-button">Login</Button>
